48fbf40f38b62989883226d9d6ea035fde4fc3de
Tracy Ling <112430369+lingt6197@users.noreply.github.com>
2026-04-28 23:01:49 -0500

    Fixed queries.sql file

--- src/queries.sql
+++ src/queries.sql
@@ -108,3 +108,20 @@ LEFT JOIN
   mirrored_repos m
 ON
   LOWER(r.repo_name) = LOWER(m.repo_name)
+
+
+-- For querying user profiles for geocoding:
+SELECT
+  actor.login,
+  created_at,
+  type,
+  EXTRACT(MONTH FROM created_at) AS month,
+  EXTRACT(DAY FROM created_at) AS day,
+  EXTRACT(YEAR FROM created_at) AS year
+FROM `githubarchive.year.2015` -- adjust the year as needed
+WHERE actor.login IS NOT NULL
+AND NOT REGEXP_CONTAINS(
+    LOWER(actor.login),
+    r'(\[bot\]|bot\b|\bbot\b)'
+)
+LIMIT 1000000
